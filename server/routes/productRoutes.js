de624a4d260d6933da66ea9b51594cff471c7a7a
zeemurph <zgmurphy@gmail.com>
2026-03-17 18:00:08 -0400

    sadfdsaf

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -10,11 +10,13 @@ router.get("/products", async (req, res) => {
 router.get("/products/:id", async (req, res) => {
   const product = await Product.findById(req.params.id);
   res.json(product);
+  console.log("Fetched product:", product.productName);
 });
 
 router.get("/products/barcode/:barcode", async (req, res) => {
   const product = await Product.findOne({ barcode: req.params.barcode });
   res.json(product);
+  console.log("Fetched product:", product.productName);
 });
 
 router.post("/products", async (req, res) => {
