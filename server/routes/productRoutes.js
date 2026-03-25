e2ed550f222379b6614e73ffd39f01e232bb3b83
zeemurph <zgmurphy@gmail.com>
2026-03-25 16:23:37 -0400

    messages

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -4,13 +4,14 @@ const Product = require("../models/Product");
 
 router.get("/products", async (req, res) => {
   const products = await Product.find();
+  console.log(`Fetched ${products.length} products from database`);
   res.json(products);
 });
 
 router.get("/products/:id", async (req, res) => {
   const product = await Product.findById(req.params.id);
   res.json(product);
-  console.log("Fetched product:", product);
+  console.log("Fetched product:", product.productName);
 });
 
 router.get("/products/barcode/:barcode", async (req, res) => {
